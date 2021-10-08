b99b7b0291c203efb8127c0afe4624727fe611a8
Romeo <baobab_man@kakao.com>
2021-10-08 14:17:52 +0900

    프로젝트 생성

---
+++ src/main/java/spring_project/board/controller/BoardController.java
@@ -0,0 +1,31 @@
+package spring_project.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write() {
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+}
