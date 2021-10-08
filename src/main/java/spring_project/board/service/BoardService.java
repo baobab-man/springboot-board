f8774187348c6cf5f8c536ea86899e2e99be64dc
Romeo <baobab_man@kakao.com>
2021-10-08 20:59:42 +0900

    페이징

--- src/main/java/spring_project/board/service/BoardService.java
+++ src/main/java/spring_project/board/service/BoardService.java
@@ -1,5 +1,8 @@
 package spring_project.board.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import spring_project.board.domain.entity.Board;
 import spring_project.board.domain.repository.BoardRepository;
@@ -14,6 +17,8 @@ import java.util.Optional;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private static final int BLOCK_PAGE_NUM_COUNT = 5; //블럭에 존재하는 페이지 수
+    private static final int PAGE_POST_COUNT = 3;  //한 페이지에 존재하는 게시글 수
 
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
@@ -49,8 +54,12 @@ public class BoardService {
     }
 
     @Transactional
-    public List<BoardDto> getBoardlist() {
-        List<Board> boards = boardRepository.findAll();
+    public List<BoardDto> getBoardlist(Integer pageNum) {
+
+        Page<Board> page = boardRepository
+                .findAll(PageRequest
+                        .of(pageNum-1, PAGE_POST_COUNT, Sort.by(Sort.Direction.ASC, "createdDate")));
+        List<Board> boards = page.getContent();
         List<BoardDto> boardDtoList = new ArrayList<>();
 
         for(Board board : boards) {
@@ -87,5 +96,34 @@ public class BoardService {
     @Transactional
     public void deletePost(Long id) {
         boardRepository.deleteById(id);
+    }
+
+    public Integer[] getPageList(Integer curPageNum) {
+        Integer[] pageList = new Integer[BLOCK_PAGE_NUM_COUNT];
+
+        // 총 게시글 수
+        Double postsTotalCount = Double.valueOf(this.getBoardCount());
+
+        // 총 게시글 수를 기준으로 계산한 마지막 페이지 번호 계산
+        Integer totalLastPageNum = (int)(Math.ceil((postsTotalCount/PAGE_POST_COUNT)));
+
+        // 현재 페이지를 기준으로 블럭의 마지막 페이지 번호 계산
+        Integer blockLastPageNum = (totalLastPageNum > curPageNum + BLOCK_PAGE_NUM_COUNT)
+                ? curPageNum + BLOCK_PAGE_NUM_COUNT
+                : totalLastPageNum;
+
+        // 페이지 시작 번호 조정
+        curPageNum = (curPageNum<=3) ? 1 : curPageNum-2;
+
+        // 페이지 번호 할당
+        for(int val=curPageNum, i=0;val<=blockLastPageNum;val++, i++) {
+            pageList[i] = val;
+        }
+        return pageList;
+    }
+
+    @Transactional
+    public Long getBoardCount() {
+        return boardRepository.count();
     }
 }
