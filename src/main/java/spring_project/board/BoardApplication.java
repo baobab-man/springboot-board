a03a5b1eeadedfc3e7581b9bc18f56b7871fc8dc
Romeo <baobab_man@kakao.com>
2021-10-08 18:21:54 +0900

    게시글 수정 및 삭제

--- src/main/java/spring_project/board/BoardApplication.java
+++ src/main/java/spring_project/board/BoardApplication.java
@@ -2,14 +2,27 @@ package spring_project.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BoardApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
     }
 
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
--- src/main/java/spring_project/board/controller/BoardController.java
+++ src/main/java/spring_project/board/controller/BoardController.java
@@ -2,9 +2,7 @@ package spring_project.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import spring_project.board.dto.BoardDto;
 import spring_project.board.service.BoardService;
 
@@ -44,5 +42,25 @@ public class BoardController {
         BoardDto boardDto = boardService.getPost(id);
         model.addAttribute("boardDto", boardDto);
         return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long id) {
+        boardService.deletePost(id);
+        return "redirect:/";
     }
 }
